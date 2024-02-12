fb2361bbdc4317d4eaf28eb01f13e098a6c7931a
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-02-12 16:01:24 +0100

    feat: Add errors logs

--- src/app/api/inscription/route.ts
+++ src/app/api/inscription/route.ts
@@ -4,45 +4,53 @@ import axios from "axios";
 export const dynamic = "force-dynamic";
 
 const notion = (data: InscriptionCommand) => {
-  return axios.post(
-    "https://api.notion.com/v1/pages",
-    {
-      parent: {
-        type: "database_id",
-        database_id: process.env.NOTION_DB_INSCRIPTIONS,
-      },
-      properties: {
-        Email: {
-          type: "title",
-          title: [{ type: "text", text: { content: data.email } }],
+  try {
+    return axios.post(
+      "https://api.notion.com/v1/pages",
+      {
+        parent: {
+          type: "database_id",
+          database_id: process.env.NOTION_DB_INSCRIPTIONS,
+        },
+        properties: {
+          Email: {
+            type: "title",
+            title: [{ type: "text", text: { content: data.email } }],
+          },
         },
       },
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
-        "Notion-Version": "2022-06-28",
-      },
-    }
-  );
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
+          "Notion-Version": "2022-06-28",
+        },
+      }
+    );
+  } catch (e) {
+    console.error("Erreur notion -----------", e);
+  }
 };
 
 const brevo = (data: InscriptionCommand) => {
-  return axios.post(
-    "https://api.brevo.com/v3/contacts",
-    {
-      email: data.email,
-      listIds: [Number(process.env.BREVO_LIST_ID)],
-      updateEnabled: false,
-    },
-    {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "api-key": process.env.BREVO_API_KEY,
+  try {
+    return axios.post(
+      "https://api.brevo.com/v3/contacts",
+      {
+        email: data.email,
+        listIds: [Number(process.env.BREVO_LIST_ID)],
+        updateEnabled: false,
       },
-    }
-  );
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+        },
+      }
+    );
+  } catch (e) {
+    console.error("Erreur brevo -----------", e);
+  }
 };
 
 export async function POST(request: Request) {
